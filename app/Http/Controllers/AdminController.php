d20b2772863ece2f7aba3467e454569632b62118
Renn <ppskdos@gmail.com>
2025-02-08 15:43:35 +0800

    push to github

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        if (Auth::id()) {
+            $role = Auth::user()->role;
+            if ($role == 'user') {
+                return view('dashboard');
+            } elseif ($role == 'admin') {
+                return view('admin.index');
+            } else {
+                return redirect()->back();
+            }
+        }
+    }
+}
